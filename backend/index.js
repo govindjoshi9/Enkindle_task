cb3bd9f6baa5ca6103491f6c2f55a7c64d7ce6b9
govindjoshi9 <joshigovind740@gmail.com>
2026-02-18 23:26:51 +0530

    db connection done

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const cors = require("cors");
-const json = require("jsonwebtoken");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+connectDB();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const app = express();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
