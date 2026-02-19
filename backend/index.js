7f09714288665cc10d86963d56f1c60781f4674f
govindjoshi9 <joshigovind740@gmail.com>
2026-02-20 00:01:47 +0530

    backend server setting

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
